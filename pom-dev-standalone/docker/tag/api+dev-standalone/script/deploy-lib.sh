d2291023e8dbab8d2ef4b36b3861258c810208d9
huawei laptop <huawei@laptop.com>
2021-09-22 12:25:59 +0800

    pass dev-standalone docker build

--- pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy-lib.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy-lib.sh
@@ -1,134 +1,107 @@
 #!/bin/sh
 ################################
 ## define your var for lib deploy
-# export DL_ROLLBACK='env-test-saas-1.0.0-standalone.jar'
-# export DL_DOCKERNAME='env-api'
 ################################
-rollback=${DL_ROLLBACK}
-dockername=${DL_DOCKERNAME}
 app='app.jar'
-keep=7 ## one week
-webapps='/webapps'
-#
-#
-## check environemnt
-if [ ! $rollback ];then
-  echo environment DL_ROLLBACK has not yet exported
-  exit
-fi
 
+JAR_BIN=$(which jar)
 
-## check files
-cd $webapps
-if [ ! -d ./lib ];then
-  echo './lib dir not found !'  >/dev/stderr  ## stderr
-  exit
-fi
-if [ -z $(ls ./lib) ];then
-  echo 'no lib found under ./lib !' >/dev/stderr  ## stderr
-  exit
-fi
+putlocaljars() {
+  app=$1
+  libroot=$2
 
-## check app.jar, if no app.jar, then app=*-standalone.jar
-if [ ! -f $app ];then 
-   app=$(ls *-standalone.jar)
-   ## found
-   if [ $#==0 ];then
-      echo 'no file matches pattern *-standalone.jar found !' >/dev/stderr
-      exit
-   fi
-fi
+  num=0
+  for lib in $(ls $libroot)
+  do
+     num=$(($num+1))
+     jar=$libroot/$lib
 
-## start fix
-fixapp='app-fix.jar'
-if [ ! -f lib/$fixapp ];then
-   echo cp $app lib/$fixapp
-   cp $app lib/$fixapp
-fi
-
-
-##
-## iterate lib
-cd lib
-num=0
-#
-for lib in $(ls)
-do
-   num=$(($num+1))
-   inf=$(jar tf $fixapp | grep $lib)
-   if [ $inf ];then
-     ## update lib
-     echo $inf
-     inf_dir=$(dirname $inf)
-     if [ ! -d $inf_dir ];then
-        mkdir -p $inf_dir
+     jarlib=$(basename $lib)
+     jarok=$("$JAR_BIN" tf $app | grep $jarlib)
+     echo jarlib=$jarlib, jarok=$jarok
+     if [ ! $jarok ];then
+         ## means new jar
+         echo "+ BOOT-INF/lib/$jarlib"
+         jarok="BOOT-INF/lib/$jarlib"
      fi
-     echo mv $lib $inf_dir
-     mv $lib $inf_dir
-   fi
-done
 
-##
-if [ $num -eq 0 ];then
+     if [ $jarok ];then
+       ## update lib
+       echo $jarok
+       jardir=$(dirname $jarok)
+       if [ ! -d $jardir ];then
+          echo mkdir -p $jardir
+          mkdir -p $jardir
+       fi
+
+       # core
+       echo mv $jar $jardir
+       mv $jar $jardir
+       echo jar 0uf $app $jarok
+       "$JAR_BIN" 0uf $app $jarok
+
+       ## rm after jar updated
+       echo rm -f $jarok
+       rm -f $jarok
+     fi
+  done
+}
+
+search_one() {
+  pattern=$1
+
+  result=$(ls $pattern 2> /dev/null)
+  if [[ -z $result ]];then
+    echo no $pattern files found ! > /dev/stderr
+    exit
+  fi
+
+  ## error
+  num=0
+  for it in $result;do
+     num=$(($num+1))
+  done
+
+  if [ $num -eq 1 ];then
+     echo $result
+  fi
+  echo ''
+}
+
+
+## main
+if [ ! -d lib ];then
+  mkdir lib
+fi
+if [ -z $(ls lib) ];then
    echo 'no lib to deploy !' >/dev/stderr
    exit
 fi
 
+fixapp='app-fix-standalone.jar'
+if [ ! -f $fixapp ];then
+  ## check standalone.jar
+  if [ ! -f $app ];then
+    standalone=$(search_one "*-standalone.jar")
+    if [ ! $standalone ];then
+       echo 'no (or multi) -standalone.jar !' >/dev/stderr
+       exit
+    fi
 
-## deploy lib
-if [ -d BOOT-INF ];then
- for lib in $(ls BOOT-INF/lib)
- do
-   inf=$(jar tf $fixapp | grep $lib)
-   if [ $inf ];then
-      echo jar 0uf $fixapp $inf
-      jar 0uf $fixapp $inf
-   fi
- done
-fi
-## working dir
-cd ..
-pwd
-
-
-##
-## rollback
-rollback_issue=$rollback.rollback_$(date "+%m-%d")
-if [ ! -f $rollback_issue ];then
-   echo mv $app $rollback_issue
-   mv $app $rollback_issue
-
-   # Usage: predeploy.sh rollback keep <pattern> <num>
-   if [ -f ./predeploy.sh ];then
-      bash ./predeploy.sh rollback keep $rollback.rollback_ $keep
-   fi
-fi
-
-##
-#deploy
-echo mv lib/$fixapp $app
-if [ ! ${DEBUG} ];then
-   mv lib/$fixapp $app
-fi
-
-
-#-------
-#cleanup 
-echo rm -rf lib/*
-if [ ! ${DEBUG} ];then
-   rm -rf lib/*
-fi
-
-
-#------
-#restart docker container only when dockername is defined
-if [ $dockername ];then
-  echo docker-compose restart $dockername
-  if [ ! ${DEBUG} ];then
-     docker-compose restart $dockername
-     #docker-compose -f docker-compose.yml restart redis
+    ## start
+    echo mv $standalone $app
+    mv $standalone $app
   fi
+
+   echo cp $app $fixapp
+   cp $app $fixapp
+
+   echo putlocaljars $fixapp lib
+   putlocaljars $fixapp lib
 fi
 
-echo success
+## clean up
+if [ -d BOOT-INF ];then
+  rm -rf BOOT-INF/
+fi
 
--- pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy.sh
@@ -2,34 +2,70 @@
 ################################
 #export ROLLBACK_KEEP_NUM=2
 ################################
+keep=${ROLLBACK_KEEP_NUM}  # keep rollback instances
 
-## rollback
-keep=${ROLLBACK_KEEP_NUM}  # keep 7 rollback instances
-app= 'app.jar'
-
-## main
-rollback=${ls *-standalone.jar}
-ROLLBACK=$rollback.rollback_$(date "+%m-%d")
+## global definition
+app='app.jar'
 
 rollback() {
-   if [ ! -f $ROLLBACK ];then
-      echo cp $app $ROLLBACK
-      cp $app $ROLLBACK
-      echo predeploy.sh rollback keep ${rollback}.rollback_ $keep
-      predeploy.sh rollback keep ${rollback}.rollback_ $keep
-   fi
+  app=$1
+  rollback=$2
+
+  if [ ! -f $app ];then
+     echo mv $rollback $app
+     mv $rollback $app
+     echo done!
+     exit
+  fi
+
+  ROLLBACK=$app.rollback_$(date "+%m-%d")
+  if [ ! -f $ROLLBACK ];then
+     echo cp $app $ROLLBACK
+     cp $app $ROLLBACK
+     echo predeploy.sh rollback keep $app.rollback_ $keep
+     predeploy.sh rollback keep $app.rollback_ $keep
+  fi
 }
 
+search_one() {
+  pattern=$1
 
-if [ ! $rollback ];then
-   echo no standalone, no need to rollback
+  result=$(ls $pattern 2> /dev/null)
+  if [[ -z $result ]];then
+    echo no $pattern files found ! > /dev/stderr
+    exit
+  fi
+
+  ## error
+  num=0
+  for it in $result;do
+     num=$(($num+1))
+  done
+
+  if [ $num -eq 1 ];then
+     echo $result
+  fi
+  echo ''
+}
+
+## main
+rollback=$(ls *-standalone.jar 2> /dev/null)
+if [ -z $rollback ];then
+   echo no *-standalone.jar, no need to rollback ! >/dev/stderr
    exit
 fi
 
-## first deploy
-if [ ! -f $app ];then
-   echo mv $rollback $app
-   mv $rollback $app
-else
-   rollback
+rollback=$(search_one "*-standalone.jar")
+if [ ! $rollback ];then
+   echo 'no (or multi) -standalone.jar !' >/dev/stderr
+   exit
 fi
+
+## rollback first
+echo => start to rollback:  $app $rollback
+rollback $app $rollback
+
+## deploy
+echo mv $rollback $app
+mv $rollback $app
+
