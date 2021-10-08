8bf3bde76218f24a2106c1fb485fd2b15d09e510
huawei laptop <huawei@laptop.com>
2021-10-08 12:22:36 +0800

    change /bin/sh => /usr/bin/env bash

--- pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy-lib.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/deploy-lib.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 ################################
 ## define your var for lib deploy
 ################################
--- pom-dev-standalone/docker/tag/api+dev-standalone/script/entrypoint.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/entrypoint.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 ## keep workding_dir from docker-compose.yml
 
 echo deploy-lib.sh ...
