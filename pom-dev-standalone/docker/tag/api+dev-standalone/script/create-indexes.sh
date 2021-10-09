a8781ea964179f475e0c453b0990038109d68888
huawei laptop <huawei@laptop.com>
2021-10-09 12:52:52 +0800

    create-indexes.sh

--- pom-dev-standalone/docker/tag/api+dev-standalone/script/create-indexes.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/create-indexes.sh
@@ -15,63 +15,79 @@ fi
 JAR_BIN=$(which jar)
 
 createjarindexes(){
+   jarpath=$1
+   indexesroot=$2
+
+   entries=$("$JAR_BIN" -tf $jarpath)
+
+   ## start
+   jarname=$(basename $jarpath)
+   for entry in $entries;do
+#   echo $entry
+
+        ## get file name
+        entrypath=$(dirname $entry)
+        entryname=$(basename $entry)
+        if [ ! $entryname ];then
+           continue
+        fi
+        ext=${entryname##*.}
+        if [ ! $ext ];then
+          continue
+        fi
+        entrynew=$entrypath/$entryname
+
+        if [ $ext = 'jar' -o $ext = 'class' ];then
+            echo "$entryname,$entrynew,$jarname"
+
+            firstletter=${entryname::1}  ##first letter
+            #firstletter=${firstletter,,}  ##lower case for bash >=4.0
+            if [ -f $indexesroot/$firstletter ];then
+                echo "$entryname,$entrynew,$jarname" >> $indexesroot/$firstletter
+            else
+                echo "$entryname,$entrynew,$jarname" > $indexesroot/$firstletter
+            fi
+        fi
+    done
+}
+
+
+createjarindexes_with_entries(){
    jar=$1
    pattern=$2
    indexesroot=$3
 
-   unset entries
-   if [ $pattern ];then
-      entries=$("$JAR_BIN" -tf $jar | grep $pattern)
-   elif [ $pattern = '*' ];then
-      entries=$("$JAR_BIN" -tf $jar)
-   fi
-   echo $entries
-   exit
+   entries=$("$JAR_BIN" -tf $jar | grep $pattern)
 
    for entry in $entries;do
+#   echo $entry
+
         ## get file name
-        filename=${entry##*/}
-        if [ ! $filename ];then
+        entrypath=$(dirname $entry)
+        entryname=$(basename $entry)
+        if [ ! $entryname ];then
            continue
-        fi    
-        ext=${filename##*.}
+        fi
+        ext=${entryname##*.}
+        entrynew=$entrypath/$entryname
 
         if [ $ext = 'jar' ];then
-            echo $entry
-
-            #echo $entry
+#            echo $entry
             ## extra it
             if [ ! -f $entry ];then
-               jar xf $jar $entry
+#               echo $entrynew
+               "$JAR_BIN" -xf $jar $entrynew
             fi
 
-            indexes=$(jar tf $entry);
-            for index in $indexes;do
-                name=${index##*/}
-                if [ ! $name ];then
-                   continue
-                fi
-                firstletter=${name::1}  ##first letter
-                #firstletter=${firstletter,,}  ##lower case for bash >=4.0
-                # lower
-                posfix=${name##*.}
-
-                if [ $posfix = 'class' ];then
-                    echo $name
-                    if [ -f $indexesroot/$firstletter ];then
-                        echo "$name,$index,$entry" >> $indexesroot/$firstletter
-                    else 
-                        echo "$name,$index,$entry" > $indexesroot/$firstletter
-                    fi
-                fi
-            done
+            if [ -f $entrynew ];then
+                echo ... $entrynew
+                createjarindexes $entrynew $indexesroot
+            fi
         fi
-        #echo $entry
     done
 }
 
 
 ## main
-patternall='*'
-createjarindexes $jar $patternall $indexes
-#createjarindexes $jar $entryPattern $indexes
+createjarindexes $jar $indexes
+createjarindexes_with_entries $jar $entryPattern $indexes
