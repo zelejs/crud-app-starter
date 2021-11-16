2265fe39fd46fa21b267076c93e2069306155e8d
vincent@macbook <vincent@macbook.air>
2021-11-16 20:18:00 +0800

    update entrypoint.sh for -standalone.jar deploy

--- pom-dev-standalone/docker/build-image.sh
+++ pom-dev-standalone/docker/build-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker image rm zelejs/api:dummy
-docker image rm api:dummy
+#docker image rm zelejs/api:dummy
+#docker image rm api:dummy
 
 docker-compose -f docker+buildimage.yml build --force-rm --no-cache
