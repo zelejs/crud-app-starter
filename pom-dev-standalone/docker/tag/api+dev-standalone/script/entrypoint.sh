b72d0b87062632d9f2538ae1e381a17a8609d038
vincent@macbook <vincent@macbook.air>
2021-11-17 21:57:22 +0800

    update entrypoint.sh

--- pom-dev-standalone/docker/tag/api+dev-standalone/script/entrypoint.sh
+++ pom-dev-standalone/docker/tag/api+dev-standalone/script/entrypoint.sh
@@ -4,22 +4,126 @@
 # deploy   -  deploy the classes, libs, standalone only without restart docker container
 # restart  -  deploy the classes, libs, standalone only with restarting docker container
 # dummy    -  deploy only without restart, and go on startup dummy api
+## real rollback app
+checksame(){
+   app1=$1
+   app2=$2
+   
+   app1_sum=$(sha256sum $app1)
+   app1_sum=${app1_sum%%\ *}
+
+   app2_sum=$(sha256sum $app2)
+   app2_sum=${app2_sum%%\ *}
+
+   if [[ $app1_sum = $app2_sum ]];then
+       echo $app1_sum 
+   fi
+}
+
+
+rollback() {
+   local app_war=$1
+   app_dir=${app_war%\/*}
+   app_name=${app_war##*\/}
+   app_sum=$(sha256sum $app_war)
+   app_sum=${stand_sum%%\ *}
+
+   ##### dayly
+   ROLLBACK=$app_name.rollback_$(date "+%m-%d")
+   if [ ! -f $app_dir/$ROLLBACK ];then
+       echo cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       echo $ROLLBACK
+       return
+   fi
+   ## ROLLBACK exists, check if sha256 the same
+   shasum=$(checksame $app_war $app_dir/$ROLLBACK)
+   if [ $shasum ];then
+       # is the same jar, just return 
+       return 
+   fi
+
+
+   ##### hourly
+   ## means not the same file, get the hourly ROLLBACK
+   ROLLBACK=$app_name.rollback_$(date "+%m-%d-%H")
+   if [ ! -f $app_dir/$ROLLBACK ];then
+       echo cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       echo $ROLLBACK
+       return
+   fi
+   ## ROLLBACK exists, check if sha256 the same
+   shasum=$(checksame $app_war $app_dir/$ROLLBACK)
+   if [ $shasum ];then
+      # is the same jar, just return 
+       return 
+   fi
+
+
+   ##### minutely
+   ## means not the same file, get the minutely ROLLBACK
+   ROLLBACK=$app_name.rollback_$(date "+%m-%d-%H-%M")
+   if [ ! -f $app_dir/$ROLLBACK ];then
+       echo cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       cp $app_war $app_dir/$ROLLBACK > /dev/stderr
+       echo $ROLLBACK
+       return
+   fi
+   ## ROLLBACK exists, check if sha256 the same
+   shasum=$(checksame $app_war $app_dir/$ROLLBACK)
+   if [ $shasum ];then
+      # is the same jar, just return 
+       return 
+   fi
+
+   ##### still the same
+   echo "$ROLLBACK exists, pls wait for one more minute !" > /dev/stderr
+}
+
+
 
 echo get started .............................
 ## prepare /webapps/lib, /webapps/classes
 if [ -d /var/webapps/classes ];then 
-   if [ ! -z $(ls /var/webapps/classes/*.class) ];then 
+   if [ ! -z $(ls /var/webapps/classes/*.class 2> /dev/null) ];then 
       mv /var/webapps/classes/*.class /webapps/classes
    fi
 fi
 if [ -d /var/webapps/lib ];then 
-   if [ ! -z $(ls /var/webapps/lib/*.jar) ];then 
+   if [ ! -z $(ls /var/webapps/lib/*.jar 2> /dev/null) ];then 
       mv /var/webapps/lib/*.jar /webapps/lib
    fi
 fi
 if [ -d /var/webapps/app ];then 
-   if [ ! -z $(ls /var/webapps/app/*-standalone.jar) ];then 
-      mv /var/webapps/app/*-standalone.jar /webapps
+   if [ ! -z $(ls /var/webapps/app/*-standalone.jar 2> /dev/null) ];then
+      # check filename the same
+      standalones=$(ls /var/webapps/app/*-standalone.jar)
+      apps=$(ls /webapps/*-standalone.jar)
+      standalonel=$standalones
+      appl=$apps
+      standalone_name=${standalonel##*\/}  ##basename
+      app_name=${appl##*\/}  ##basename
+
+      if [[ $standalone_name = $app_name ]];then
+         shasum=$(checksame $standalonel $appl)
+         if [ $shasum ];then
+            echo "the sample jar: $standalone_name ... do nothing !" > /dev/stderr
+         else 
+            ## difference checksum sha256, roll it back
+            rollback $appl
+
+            ## copy the standalone to /webapps
+            sha256sum $appl
+            echo cp $standalonel $appl
+            cp $standalonel $appl
+            sha256sum $appl
+         fi
+      else
+         ## not the same file name, just move the jar file
+         echo mv /var/webapps/app/*-standalone.jar /webapps > /dev/stderr
+         mv /var/webapps/app/*-standalone.jar /webapps
+      fi
    fi
 fi
 
