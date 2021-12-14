f391c93d7296ba43354c66cb0714db652b2adf67
m.2@sandbox <m.2@sandbox>
2021-12-14 15:25:22 +0800

    test pass pom-dev-starter

--- pom-dev-starter/src/main/java/com/jfeat/AmApplication.java
+++ pom-dev-starter/src/main/java/com/jfeat/AmApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AmApplication extends WebMvcConfigurerAdapter  {
     protected final static Logger logger = LoggerFactory.getLogger(AmApplication.class);
 
@@ -21,6 +22,6 @@ public class AmApplication extends WebMvcConfigurerAdapter  {
 
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
-        logger.info("Webjar run success!");
+        logger.info("dev-starter run success!");
     }
 }
