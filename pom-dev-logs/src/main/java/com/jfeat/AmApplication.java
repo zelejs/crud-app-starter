2e9bec786ca32b3a9d821b48315168b5073d177c
zhuhantao <1799231213@qq.com>
2022-04-27 15:16:54 +0800

    业务优化，bug修复

--- pom-dev-logs/src/main/java/com/jfeat/AmApplication.java
+++ pom-dev-logs/src/main/java/com/jfeat/AmApplication.java
@@ -1,5 +1,7 @@
 package com.jfeat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
