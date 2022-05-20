ed856e5738e7283da94ed34df0b82fe83015aa87
HDJ <263463764@qq.com>
2022-05-20 10:08:53 +0800

    pom-dev-navigation更新nav-ui代码

--- pom-dev-navigation/web/src/pages/nagation/layout.js
+++ pom-dev-navigation/web/src/pages/nagation/layout.js
@@ -45,6 +45,7 @@ module.exports = {
   container: 'SimCRUDList',
   navigation: {
     model: {
+      delComfirmTips: true,
       api: {
         createAPI: '/api/pub/data/services/navigation',
         getAPI: '/api/pub/data/services/navigation/(id)',
@@ -55,52 +56,62 @@ module.exports = {
         label: '标题',
         field: 'name',
         type: 'input',
-        required: {
-          placeholder: '请输入标题'
+        rules: {
+          isRequired: true
+        },
+        props: {
+          placeholder: '请输入标题',
         }
       },
       {
         label: '图片',
         field: 'url',
         type: 'input',
-        required: {
-          placeholder: '图片链接'
+        rules: {
+          isRequired: true
+        },
+        props: {
+          placeholder: '请输入图片',
+        }
+      },
+      {
+        label: '链接',
+        field: 'path',
+        type: 'input',
+        rules: {
+          isRequired: true
+        },
+        props: {
+          placeholder: '请输入本地链接/第三方以http开头'
+        }
+      },
+      {
+        label: '类别',
+        field: 'typeId',
+        type: 'select-fetch',
+        rules: {
+          isRequired: true
+        },
+        props: {
+          placeholder: '请类别'
+        },
+        saveData: { //额外提交的字段和值
+          typeName: 'name'
+        },
+        options: {
+          api: '/api/pub/data/services/navCategory',
+          label: 'name',
+          value: 'id',
         }
       },
       {
         label: '描述',
         field: 'desc',
         type: 'input',
-        // height: '25px',
-        required: {
+        props: {
           placeholder: '描述属性'
         }
-      }
-        ,
-      {
-        label: '链接',
-        field: 'path',
-        type: 'input',
-        required: {
-          placeholder: '请输入本地链接/第三方以http开头'
-        }
-      }, {
-        label: '类别',
-        field: 'typeId',
-        type: 'select-fetch',
-        required: {
-          placeholder: '请类别'
-        },
-        saveData: {
-          //额外提交的字段和值
-          typeName: 'name'
-        },
-        options: {
-          api: '/api/pub/data/services/navCategory',
-          label: 'name',
-          value: 'id'
-        }
-      }
+      },
       ]
     }
   }
