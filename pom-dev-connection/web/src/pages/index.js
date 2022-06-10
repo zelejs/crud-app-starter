921f4f84d59714b03c0624edf83b0c3ec3c957ad
HDJ <263463764@qq.com>
2022-06-10 11:25:31 +0800

    pom-dev-connection 新增 web 前端代码

---
+++ pom-dev-connection/web/src/pages/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import Connection from '@/pages/Connection'
+
+export default function index (props) {
+
+  return (
+    <Connection {...props} />
+  )
+
+}
