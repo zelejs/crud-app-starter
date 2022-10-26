f542c3bc72fb29f389db358a0094b91b734a3e68
m.2@sandbox <m.2@sandbox>
2022-10-26 17:06:30 +0800

    add dependency-ui.html for WebMvcConfigurerAdapter

--- pom-dev-dependency/src/main/java/com/jfeat/AmApplication.java
+++ pom-dev-dependency/src/main/java/com/jfeat/AmApplication.java
@@ -20,9 +20,14 @@ public class AmApplication extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    
+        // dependency-ui.html
+        registry.addResourceHandler("dependency-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/dependency-ui/**").addResourceLocations("classpath:/META-INF/resources/dependency-ui/");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
     }
 }
+
