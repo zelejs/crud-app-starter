cbbc80b36c564eccf25e4c1abaa50c09529fb7e1
tmp <you@email>
2023-10-30 14:22:38 +0800

    update pom-dev-dependency with EnableSwagger2

--- pom-dev-dependency/src/main/java/com/jfeat/AmApplication.java
+++ pom-dev-dependency/src/main/java/com/jfeat/AmApplication.java
@@ -1,11 +1,11 @@
 package com.jfeat;
 
 import com.jfeat.jar.dep.properties.JarDeployProperties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -16,25 +16,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author zxchengb
  */
 @SpringBootApplication
+@EnableSwagger2
 public class AmApplication extends WebMvcConfigurerAdapter {
-    static Logger logger = LoggerFactory.getLogger(AmApplication.class);
+    static Logger log = LoggerFactory.getLogger(AmApplication.class);
 
-    /**
-     * 增加swagger的支持
-     */
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
 //        // dependency-ui.html
 //        registry.addResourceHandler("dependency-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 //        registry.addResourceHandler("/dependency-ui/**").addResourceLocations("classpath:/META-INF/resources/dependency-ui/");
 //    }
 
     public static void main(String[] args) {
-        logger.info("dev-dependency app run success !");
         SpringApplication.run(AmApplication.class, args);
+        log.info("dev-dependency app run success !");
     }
 }
 
