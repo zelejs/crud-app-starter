4d2169d52c550dc5b988ffce453a14c382d869f3
vincent@local.com <vincent@local.com>
2024-11-28 17:38:32 +0800

    remove pom-dev-preview, add org.version for pom-parent

--- pom-dev-preview/web/public/config.js
+++
@@ -1,7 +0,0 @@
-
-if (window.ZEle === undefined) {
-    window.ZEle = {};
-}
-// window.ZEle.endpoint = 'http://localhost.static.smallsaas.cn'
-// window.ZEle.endpoint = 'http://localhost:8080'
-window.ZEle.endpoint = ''
